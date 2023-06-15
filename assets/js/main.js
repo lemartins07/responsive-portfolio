fbaa1fe13e52dc21737a626d4fa8a9b55aae15b2
Leandro Martins <leandro.marttins.85@gmail.com>
2023-06-15 18:02:39 -0300

    Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,21 @@ modalCloses.forEach(modalClose => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+const swiper = new Swiper('.portfolio__container', {
+  cssMode: true,
+  loop: true,
 
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  mousewheel: true,
+  keyboard: true,
+})
 
 /*==================== TESTIMONIAL ====================*/
 
